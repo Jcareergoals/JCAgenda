e14c415a7269e93026113214a2c14b8eff814fc5
Jose Chery <Jcareergoals@hotmail.com>
2016-06-28 00:48:36 -0400

    Refactored 'user' model

--- application/models/user.php
+++ application/models/user.php
@@ -3,7 +3,7 @@
 class user extends CI_Model {
 	public function __construct()
 	{
-		parent::__construct();
+		parent::__construct(); 
 		$this->load->library('form_validation');
 		function validate($data)
 		{
@@ -19,41 +19,54 @@ class user extends CI_Model {
 	{
 		if ($data['form'] == 'registration' )
 		{
-			$this->form_validation->set_rules('first_name', 'First Name', 'required|min_length[3]|max_length[45]|trim|ucwords');
-			$this->form_validation->set_rules('last_name', 'Last Name', 'required|min_length[3]|max_length[45]|trim|ucwords');
-			$this->form_validation->set_rules('username', 'User Name', 'required|min_length[3]|max_length[45]|trim|ucwords');
+			$this->form_validation->set_rules('name', 'Name', 'required|min_length[3]|max_length[45]|trim|ucwords');
+			$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email'); 
 			$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|max_length[45]|trim|matches[confirm_password]|md5');
 			$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|trim|md5');
-			return validate($this->form_validation);
-			
+			$this->form_validation->set_rules('dob', 'Date of Birth', 'required|min_length[10]|max_length[10]');
+			return validate($this->form_validation);		
 		} 
 		else if ($data['form'] == 'login')
 		{
-			$this->form_validation->set_rules('username_2', 'User Name', 'required|ucwords|min_length[3]|trim');
+			$this->form_validation->set_rules('email_2', 'Email', 'required|trim|valid_email');
 			$this->form_validation->set_rules('password_2', 'Password', 'required|min_length[8]|trim|md5');
 			return validate($this->form_validation);
 		}
-		// else if ($data['form'] == 'trip')
-		// {
-		// 	$this->form_validation->set_rules('destination', 'Destination', 'required|min_length[3]|max_length[45]|trim|ucwords');
-		// 	$this->form_validation->set_rules('description', 'Description', 'required|min_length[3]|trim|ucfirst');
-		// 	$this->form_validation->set_rules('from_date', 'Travel Date From', 'required|min_length[10]|max_length[10]|trim');
-		// 	$this->form_validation->set_rules('to_date', 'Travel Date To', 'required|min_length[10]|max_length[10]|trim');
-		// 	return validate($this->form_validation);
-		// }
 	}
 	public function register($user)
 	{
-		$query = "INSERT INTO users (first_name, last_name, username, password, created_at, updated_at)
+		$query = "INSERT INTO users (name, email, password, dob, created_at, updated_at)
 				  VALUES (?,?,?,?,NOW(),NOW())";
-		$values = array($user['first_name'], $user['last_name'], $user['username'], $user['password']);
+		$values = array($user['name'], $user['email'], $user['password'], $user['dob']);
 		return $this->db->query($query, $values);
 	}
-	public function get_user_by_username($post)
+	// used in registration and login
+	public function get_user_by_email($data)
 	{
-		$query = "SELECT * FROM users WHERE username = ?";
-		$values = array($post['username']);
+		$query = "SELECT * FROM users WHERE email = ?";
+		$values = array($data['email']);
 		return $this->db->query($query, $values)->row_array();
 	}
-
+	public function check_date($data)
+	{
+		list($month, $day, $year) = explode('/', $data['dob']); 
+		var_dump($data['dob']);
+		if(!checkdate($month, $day, $year))
+		{
+			return false; 
+		} else {
+			return true; 
+		}
+	}
+	public function get_user($id)
+	{
+		$query = "SELECT 
+					    first_name, last_name, email
+					FROM
+					    users
+					WHERE
+					    id = ?";
+		$values = array($id);
+		return $this->db->query($query, $values)->row_array(); 
+	}
 }
