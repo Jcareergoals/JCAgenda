a3a2c9802e0b67dc3ae15e30c5f5d8a520b52456
Jose Chery <Jcareergoals@hotmail.com>
2016-06-29 14:38:45 -0400

    Capitalized class names

--- application/controllers/Appointments.php
+++ application/controllers/Appointments.php
@@ -1,53 +1,53 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class appointments extends CI_Controller {
+class Appointments extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct(); 
-		$this->load->model('appointment');
+		$this->load->model('Appointment');
 	}
 	public function index()
 	{
 		$user = $this->session->userdata('user'); // assign session data to user variable
-		$results = $this->appointment->get_appointments($user); // get userdata from database
+		$results = $this->Appointment->get_appointments($user); // get userdata from database
 		$this->session->set_userdata('appointments', $results); // assign appointment data to session
-		$this->load->view('appointment_profile'); 
+		$this->load->view('appointment_profile');
 	}
 	public function add()
 	{
-		$result = $this->appointment->validate($this->input->post());
+		$result = $this->Appointment->validate($this->input->post());
 		if($result == 'valid')
 		{
-			$this->appointment->add_appointment($this->input->post());
+			$this->Appointment->add_appointment($this->input->post());
 			$this->session->set_flashdata('success', "<p>Your appointment was successfully created</p>"); 
-			redirect('/appointments'); 
+			redirect('/Appointments'); 
 		} 
 		else if ($result == 'check_date')
 		{
 			$this->session->set_flashdata('errors','<p>Please select a future date</p>'); 
-			redirect('/appointments'); 
+			redirect('/Appointments'); 
 		}
 		else 
 		{
 			$this->session->set_flashdata('errors', $result);
-			redirect('/appointments');
+			redirect('/Appointments');
 		}
 	}
 	public function delete($id)
 	{
-		$this->appointment->delete_appointment($id);
-		redirect('/appointments'); 
+		$this->Appointment->delete_appointment($id);
+		redirect('/Appointments'); 
 	}
 	public function edit($id)
 	{
-		$result = $this->appointment->get_appointment($id);
+		$result = $this->Appointment->get_appointment($id);
 		$this->session->set_userdata('appointment', $result);
 		$this->load->view('appointment'); 
 	}
 	public function update($id)
 	{
-		$this->appointment->update_appointment($id, $this->input->post()); 
-		redirect('/appointments');
+		$this->Appointment->update_appointment($id, $this->input->post()); 
+		redirect("/Appointments");
 	}
 }
