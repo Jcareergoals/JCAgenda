ee8be3aca9de35e1c5702ff121146c8dcb7400c5
Jose Chery <Jcareergoals@hotmail.com>
2016-06-29 14:10:39 -0400

    Capitalized users class name: new Codeignitor convention

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'users';
-$route['appointments'] = 'appointments/index'; 
+$route['default_controller'] = 'Users';
+$route['appointments'] = 'Appointments/index'; 
 $route['404_override'] = '';
